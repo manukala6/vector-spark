d1ef5347285d239a0aea74be65fb12447625592e
Maanas Nukala <manukala6@gmail.com>
2023-09-05 21:59:37 -0700

    Perform spatial join

--- src/main/scala/Grid.scala
+++ src/main/scala/Grid.scala
@@ -1,5 +1,5 @@
 import org.apache.sedona.core.spatialRDD.SpatialRDD
-import org.locationtech.jts.geom.{Coordinate, Envelope, GeometryFactory, Polygon}
+import org.locationtech.jts.geom.{Coordinate, GeometryFactory, Polygon, PrecisionModel, Envelope}
 
 import Spark.spark
 import org.apache.spark.SparkContext
@@ -8,14 +8,16 @@ object Grid {
 
     val minLong = -180.0
     val maxLong = 180.0
-    val minLat = -90.0
-    val maxLat = 90.0
+    val minLat = -80.0
+    val maxLat = 80.0
 
+    val srid = 4326
+    
     def generate1x1(): SpatialRDD[Polygon] = {
 
         val coords = for {
-            x <- minLong to maxLong by 1.0
-            y <- minLat to maxLat by 1.0
+            x <- minLong until maxLong by 1.0
+            y <- minLat until maxLat by 1.0
         } yield (x, y)
         
         val geometryFactory = new GeometryFactory()
@@ -36,8 +38,8 @@ object Grid {
     def generate3x3(): SpatialRDD[Polygon] = {
 
         val coords = for {
-            x <- minLong to maxLong by 3.0
-            y <- minLat to maxLat by 3.0
+            x <- minLong until maxLong by 3.0
+            y <- minLat until maxLat by 3.0
         } yield (x, y)
         
         val geometryFactory = new GeometryFactory()
@@ -58,8 +60,8 @@ object Grid {
     def generate10x10(): SpatialRDD[Polygon] = {
 
         val coords = for {
-            x <- minLong to maxLong by 10.0
-            y <- minLat to maxLat by 10.0
+            x <- minLong until maxLong by 10.0
+            y <- minLat until maxLat by 10.0
         } yield (x, y)
         
         val geometryFactory = new GeometryFactory()
@@ -80,17 +82,17 @@ object Grid {
     def generate20x20(): SpatialRDD[Polygon] = {
 
         val coords = for {
-            x <- minLong to maxLong by 20.0
-            y <- minLat to maxLat by 20.0
+            x <- minLong until maxLong by 20.0
+            y <- minLat until maxLat by 20.0
         } yield (x, y)
         
-        val geometryFactory = new GeometryFactory()
-        val gridPolys = coords.map { case (x, y) => 
+        val geometryFactory = new GeometryFactory(new PrecisionModel(), srid)
+        val gridPolys = coords.flatMap { case (x, y) => 
             val envelope = new Envelope(x, x + 20, y, y + 20)
             val poly = geometryFactory.toGeometry(envelope).asInstanceOf[Polygon]
-            poly    
+            Some(poly)    
         }
-
+        
         val gridRDD = new SpatialRDD[Polygon]
         val sc: SparkContext = spark.sparkContext
         gridRDD.rawSpatialRDD = sc.parallelize(gridPolys)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,36 +1,81 @@
 import Spark.spark
+import spark.implicits._
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
-import org.apache.sedona.core.formatMapper.shapefileParser.ShapefileReader
-import org.apache.sedona.core.spatialRDD.SpatialRDD
-import org.apache.sedona.core.utils.SedonaConf
-import org.apache.sedona.sql.utils.{Adapter, SedonaSQLRegistrator}
-import org.apache.sedona.viz.core.Serde.SedonaVizKryoRegistrator
-import org.apache.sedona.viz.sql.utils.SedonaVizRegistrator
-import org.apache.spark.serializer.KryoSerializer
-import org.apache.spark.sql.SparkSession
-import org.locationtech.jts.geom.{Coordinate, Geometry, GeometryFactory}
+import org.apache.spark.sql.functions._
+import org.apache.sedona.sql.utils.Adapter
+import org.locationtech.jts.geom.{Geometry, Polygon}
+import scala.jdk.CollectionConverters._
+import org.apache.sedona.core.spatialOperator.JoinQuery
+import org.apache.sedona.core.spatialPartitioning._
+import org.apache.sedona.core.enums.GridType
+import org.apache.sedona.core.enums.IndexType
+import cats.instances.tuple
+import org.apache.sedona.sql.utils.SedonaSQLRegistrator
+
 
 object Main {
 
     def main(args: Array[String]): Unit = {
 
         // Input file URI
-        val input_file: String = "data//nems_slim.csv"
+        val input_file: String = "data//nems_slim.tsv"
 
         // Set up spark session
         val sc: SparkContext = spark.sparkContext
         val allowTopologyInvalidGeometries = true
         val skipSyntaxInvalidGeometries = false
+        sc.setLogLevel("ERROR")
+        SedonaSQLRegistrator.registerAll(spark)
         
         // Read points from TSV
-        var pointDf = spark.read.format("csv").option("delimiter","/t").option("header", "true").load(input_file)
-        println("Number of points: " + pointDf.count())
+        val rawDf = spark.read
+            .format("csv")
+            .option("header", "true")
+            .option("delimiter", "\t")
+            .load(input_file)
+        val spatialDf = rawDf.withColumn("geom", expr("ST_Point(CAST(`decimalLongitude` AS Decimal(24,20)), CAST(`decimalLatitude` AS Decimal(24,20)))"))
+        println("First five rows of spatialDf: " + spatialDf.show(5))
+        println("Number of points: " + rawDf.count())
+
+        // Convert to spatial RDD
+        val spatialRDD = Adapter.toSpatialRdd(spatialDf, "geom")
 
         // Generate 20x20 grid
         val gridRDD = Grid.generate20x20()
         println("Number of grid cells: " + gridRDD.countWithoutDuplicates())
 
+        val gridHeadRecs: List[Polygon] = gridRDD.rawSpatialRDD.take(10).asScala.toList
+        gridHeadRecs.foreach(println)
+
+        // Spatial partitioning for both RDDs
+        gridRDD.analyze()
+        gridRDD.spatialPartitioning(GridType.KDBTREE)
+        gridRDD.buildIndex(IndexType.RTREE, true)
+
+        spatialRDD.analyze()        
+        spatialRDD.spatialPartitioning(gridRDD.getPartitioner)
+        
+        // Perform spatial join
+        val result = JoinQuery.SpatialJoinQuery(spatialRDD, gridRDD, true, true)
+
+        // Group by polygon and count points
+        val counts = result.rdd
+            .map(tuple => (tuple._2, 1))
+            .reduceByKey(_ + _)
+        println("Number of grid cells with points: " + counts.count())
+        println("First five rows of counts: " + counts.take(5).toList)
+
+        // Convert to dataframe
+        val countDf = counts.map {
+            case (polygon, count) => (polygon, count)
+        }.toDF("geometry", "count")
+
+        // Write to shapefile
+        countDf.write
+            .format("shapefile")
+            .option("header", "true")
+            .save("data//nems_slim_grid_20x20.shp")
 
         sc.stop()
     }
